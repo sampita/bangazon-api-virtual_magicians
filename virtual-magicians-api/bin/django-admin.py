ff6c06dcde2ec7ec00096e696bbc5963a5d3e843
joseph walker <josephwalker504@gmail.com>
2020-02-20 13:51:21 -0600

    removed folder

--- virtual-magicians-api/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/josephwalker/workspace/PYTHON/GroupWork/bangazon-api-virtual_magicians/virtual-magicians-api/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
