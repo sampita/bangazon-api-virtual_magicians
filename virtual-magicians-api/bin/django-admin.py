acc6f1c94efd941f717f125c0151466c0f4cfbed
joseph walker <josephwalker504@gmail.com>
2020-02-20 13:15:34 -0600

    added requirements.text

---
+++ virtual-magicians-api/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/josephwalker/workspace/PYTHON/GroupWork/bangazon-api-virtual_magicians/virtual-magicians-api/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
