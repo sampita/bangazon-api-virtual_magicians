adafb73f69c237dba99d38568e8f9d46b1e11260
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-22 11:46:22 -0600

    Finished Product model

--- virtualmagicians/models/product.py
+++ virtualmagicians/models/product.py
@@ -1,8 +1,21 @@
 from django.db import models
 from .customer import Customer
+from .product_type import ProductType
 
 class Product(models.Model):
 
     name = models.CharField(max_length=50)
     customer_id = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="products")
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    location = models.CharField(max_length=75)
+    image_path = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    product_type_id = models.ForeignKey(ProductType, on_delete=models.DO_NOTHING)
+
+class Meta:
+
+    ordering = ("name", )
+    verbose_name = ("product")
+    verbose_name_plural = ("products")
